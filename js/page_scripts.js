946688a584e1e631b84f21f913389442aa827c9e
adriantiu2 <64933159+adriantiu2@users.noreply.github.com>
2020-12-27 17:25:22 -0500

    Add files via upload

---
+++ js/page_scripts.js
@@ -0,0 +1,56 @@
+var allImages = document.getElementsByTagName('img');
+
+function bigImg(x) {
+	var already = x.style.height
+	var len = allImages.length
+	for(var i = len-1; i >= 0 ; i--) {
+	  if(x.src!=allImages[i].src){
+	  	allImages[i].parentNode.removeChild(allImages[i]);
+	  }
+	}
+  if(already == "540px"){
+	location.reload();
+  }
+  else{
+  	x.style.height = "540px"
+  	x.style.top = "0px"
+  	x.style.left = "0px"
+  	var filename = x.src.match("[^.]+$")[0].toUpperCase()
+  	var title = x.src.match("([^\/]+$)")[0].match("^[^.]*")[0]
+  	var size = getFileSize(x.naturalWidth*x.naturalHeight)
+  	var back = document.createElement('button')
+  	back.innerHTML = "back"
+  	back.style.marginTop = (x.clientHeight-back.style.height)+"px"
+  	document.getElementById('main').appendChild(back);
+  	back.addEventListener ("click", function() {
+	  location.reload()
+	});
+
+  	var div = document.createElement('div');
+	var base = document.getElementById('blockOfStuff').innerHTML;
+	base = base.replace("FORMAT","Format: " + filename) 
+	base = base.replace("DIMENSIONS","Dimensions: "+x.naturalWidth+"w x "+x.naturalHeight+"h")
+	base = base.replace("SIZE", "Size: " +size)
+	base = base.replace("TITLE",title) 
+	base = base.replace("TITLE",title) 
+	base = base.replace("WIDTH",x.clientWidth) 
+	base = base.replace("HEIGHT",x.clientHeight) 
+	base = base.replace("RANDOM",getRandomInt(1000000000,10000000000))
+	div.innerHTML = base
+	document.getElementById('main').appendChild(div);
+  }
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+function getFileSize(input) {
+	if(Math.floor(input*8/1000000)==0){
+		return Math.floor(input*8/10)/1000 + "K"
+	} else{
+		return Math.floor(input*8/100000)/100 + "M"
+	}
+}
